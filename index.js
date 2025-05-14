4ab9c41747c06c5eed7247fe128a1c199e870817
urabexon <hiroki708.a48uk@gmail.com>
2025-05-14 23:05:42 +0900

    items setup onload

--- index.js
+++ index.js
@@ -11,6 +11,39 @@ window.onload = () => {
         resolution: { type: "v2", value: new THREE.Vector2() }
     };
 
+    // rendererの作成
+    renderer = new THREE.WebGLRenderer();
+
+    // canvasをbodyに追加
+    document.body.appendChild(renderer.domElement);
+
+    // canvasをリサイズ
+    renderer.setSize(windowWidth, windowHeight);
+
+    // scene作成
+    scene = new THREE.Scene();
+
+    // camera作成
+    camera = new THREE.PerspectiveCamera(75, windowWidth / windowHeight, 0.1, 1000);
+    camera.position.z = 100;
+
+    // Material作成
+    let material = new THREE.ShaderMaterial({
+    vertexShader: document.getElementById('vertexShader').textContent,
+    fragmentShader: document.getElementById('fragmentShader').textContent,
+    uniforms: uniforms
+    });
+
+    // Mesh作成
+    let mesh = new THREE.Mesh(geometry,material);
+
+    // Meshをシーンに追加
+    scene.add(mesh);
+
+    // uniformに値を格納
+    uniforms.resolution.value.x = windowWidth;
+    uniforms.resolution.value.y = windowHeight;
+
     animate();
 };
 
