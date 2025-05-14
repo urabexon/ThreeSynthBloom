271e773a35e715879f3c8a6859a00874c79c1712
urabexon <hiroki708.a48uk@gmail.com>
2025-05-14 22:51:20 +0900

    first check animate

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 var startTime = Date.now();
+var uniforms;
+var camera, scene, renderer;
 
 window.onload = () => {
+    let windowWidth = window.innerWidth;
+    let windowHeight = window.innerHeight;
 
+    uniforms = {
+        time: { type: "f", value: 1.0 },
+        resolution: { type: "v2", value: new THREE.Vector2() }
+    };
+
+    animate();
 };
+
+function animate() {
+
+}
+
+function render() {
+
+}
+
+window.onresize = () => {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
